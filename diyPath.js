b69164e4585822b0c1279babef4585a331bf1c71
琚涛 <tomatoswordsman@gmail.com>
2021-05-10 09:28:13 +0800

    删除无效文件

--- diyPath.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-module.exports = async function(filePath, savePath, markdownPath) {
-    // Return a picture access link
-    return '![](/tomatoKnightJ/Blog/blob/main/images/wave.gif?raw=true)';
-}
